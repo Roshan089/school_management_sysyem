fd593a656390fea477aa2ba41e7f5792d947983e
Roshan <1js20is089@gmail.com>
2024-09-27 03:18:11 +0530

    componentes and admin page

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        l 
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+       r
+       
+      </div>
+    </div>
+  );
+}
