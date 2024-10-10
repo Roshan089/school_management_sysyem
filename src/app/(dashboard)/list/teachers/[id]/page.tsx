4eda89644d0cda749bb53056be81689df0de0378
Roshan <1js20is089@gmail.com>
2024-10-10 13:16:30 +0530

    form model

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,10 +1,10 @@
-import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalender";
-import FormModal from "@/components/FormModal";
-import Performance from "@/components/Performance";
-import { role } from "@/lib/data";
-import Image from "next/image";
-import Link from "next/link";
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalender';
+import FormModal from '@/components/FormModal';
+import Performance from '@/components/Performance';
+import { role } from '@/lib/data';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const SingleTeacherPage = () => {
   return (
@@ -27,24 +27,26 @@ const SingleTeacherPage = () => {
             <div className="w-2/3 flex flex-col justify-between gap-4">
               <div className="flex items-center gap-4">
                 <h1 className="text-xl font-semibold">Leonard Snyder</h1>
-                {role === "admin" && <FormModal
-                  table="teacher"
-                  type="update"
-                  data={{
-                    id: 1,
-                    username: "deanguerrero",
-                    email: "deanguerrero@gmail.com",
-                    password: "password",
-                    firstName: "Dean",
-                    lastName: "Guerrero",
-                    phone: "+1 234 567 89",
-                    address: "1234 Main St, Anytown, USA",
-                    bloodType: "A+",
-                    dateOfBirth: "2000-01-01",
-                    sex: "male",
-                    img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
-                  }}
-                />}
+                {role === 'admin' && (
+                  <FormModal
+                    table="teacher"
+                    type="update"
+                    data={{
+                      id: 1,
+                      username: 'deanguerrero',
+                      email: 'deanguerrero@gmail.com',
+                      password: 'password',
+                      firstName: 'Dean',
+                      lastName: 'Guerrero',
+                      phone: '+1 234 567 89',
+                      address: '1234 Main St, Anytown, USA',
+                      bloodType: 'A+',
+                      dateOfBirth: '2000-01-01',
+                      sex: 'male',
+                      img: 'https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200',
+                    }}
+                  />
+                )}
               </div>
               <p className="text-sm text-gray-500">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -130,11 +132,12 @@ const SingleTeacherPage = () => {
           </div>
         </div>
         {/* BOTTOM */}
-        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+        <div className=" mt-4 bg-white rounded-md p-4 h-[800px]">
           <h1>Teacher&apos;s Schedule</h1>
           <BigCalendar />
         </div>
       </div>
+
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
         <div className="bg-white p-4 rounded-md">
--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client';
 
-import dynamic from "next/dynamic";
-import Image from "next/image";
-import { useState } from "react";
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import { useState } from 'react';
 
 // USE LAZY LOADING
 
 // import TeacherForm from "./forms/TeacherForm";
 // import StudentForm from "./forms/StudentForm";
 
-const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+const TeacherForm = dynamic(() => import('./forms/TeacherForm'), {
   loading: () => <h1>Loading...</h1>,
 });
-const StudentForm = dynamic(() => import("./forms/SutdentForms"), {
+const StudentForm = dynamic(() => import('./forms/SutdentForms'), {
   loading: () => <h1>Loading...</h1>,
 });
 
 const forms: {
-  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+  [key: string]: (type: 'create' | 'update', data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
-  student: (type, data) => <StudentForm type={type} data={data} />
+  student: (type, data) => <StudentForm type={type} data={data} />,
 };
 
 const FormModal = ({
@@ -30,34 +30,34 @@ const FormModal = ({
   id,
 }: {
   table:
-    | "teacher"
-    | "student"
-    | "parent"
-    | "subject"
-    | "class"
-    | "lesson"
-    | "exam"
-    | "assignment"
-    | "result"
-    | "attendance"
-    | "event"
-    | "announcement";
-  type: "create" | "update" | "delete";
+    | 'teacher'
+    | 'student'
+    | 'parent'
+    | 'subject'
+    | 'class'
+    | 'lesson'
+    | 'exam'
+    | 'assignment'
+    | 'result'
+    | 'attendance'
+    | 'event'
+    | 'announcement';
+  type: 'create' | 'update' | 'delete';
   data?: any;
   id?: number;
 }) => {
-  const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+  const size = type === 'create' ? 'w-8 h-8' : 'w-7 h-7';
   const bgColor =
-    type === "create"
-      ? "bg-lamaYellow"
-      : type === "update"
-      ? "bg-lamaSky"
-      : "bg-lamaPurple";
+    type === 'create'
+      ? 'bg-lamaYellow'
+      : type === 'update'
+        ? 'bg-lamaSky'
+        : 'bg-lamaPurple';
 
   const [open, setOpen] = useState(false);
 
   const Form = () => {
-    return type === "delete" && id ? (
+    return type === 'delete' && id ? (
       <form action="" className="p-4 flex flex-col gap-4">
         <span className="text-center font-medium">
           All data will be lost. Are you sure you want to delete this {table}?
@@ -66,10 +66,10 @@ const FormModal = ({
           Delete
         </button>
       </form>
-    ) : type === "create" || type === "update" ? (
+    ) : type === 'create' || type === 'update' ? (
       forms[table](type, data)
     ) : (
-      "Form not found!"
+      'Form not found!'
     );
   };
 
