00e02d28ee5b03df20ace1b4767124595e3b655c
Roshan <1js20is089@gmail.com>
2024-10-11 17:53:38 +0530

    create error solved

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -82,7 +82,7 @@ const ParentListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === 'admin' && <FormModal table="teacher" type="create" />}
+            {role === 'admin' && <FormModal table="parent" type="create" />}
           </div>
         </div>
       </div>
