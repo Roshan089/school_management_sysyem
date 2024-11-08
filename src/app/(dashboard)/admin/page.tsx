d9c1c6fed2666aebae9196263510915eddad91ff
Roshan <1js20is089@gmail.com>
2024-11-08 23:39:16 +0530

    user admin card

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -14,7 +14,7 @@ const AdminPage = ({
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* USER CARDS */}
+        {/* USER CARDS on admin page*/}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="admin" />
           <UserCard type="teacher" />
